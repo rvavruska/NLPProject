892c2e217d9187db6e86b608ff7251488d48584c
rvavruska <rvavruska@gmail.com>
2022-05-03 16:18:30 -0400

    add slides

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,29 +9,27 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import transformers
 from transformers import BartForConditionalGeneration, BartTokenizer
 
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-large")
+model = BartForConditionalGeneration.from_pretrained("summ_output")
 
+st.title('Article Summurization')
+summary = ""
 
-def Run_Summary(text):
-    tokenizer = BartTokenizer.from_pretrained("facebook/bart-large")
-
-    model = BartForConditionalGeneration.from_pretrained("summ_output")
-    
+def Run_Summary(text):    
     model.eval()
     input_ids = tokenizer.encode(text, return_tensors="pt")
     output_ids = model.generate(
     input_ids,
-    max_length=1024,
+    max_length=128,
     bos_token_id=tokenizer.bos_token_id,
     eos_token_id=tokenizer.eos_token_id,
     pad_token_id=tokenizer.pad_token_id,
     )
     st.write("Summary: ", tokenizer.decode(output_ids[0]))
-    return tokenizer.decode(output_ids[0])
 
-
-st.title('Article Summurization')
-text = st.text_area("Article to summarize")
-if st.button("Summarize", key=None, help=None, on_click=Run_Summary(text)):
+text = st.text_area("Article to summarize", "")
+if st.button("Summarize"):
     Run_Summary(text)
